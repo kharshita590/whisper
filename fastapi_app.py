32081243bf2ede863fd0e0589145c6eccc5af120
kharshita590 <83570759+kharshita590@users.noreply.github.com>
2025-04-01 21:24:37 +0530

    Update fastapi_app.py

--- fastapi_app.py
+++ fastapi_app.py
@@ -7,16 +7,15 @@ from tempfile import NamedTemporaryFile
 
 torch.cuda.is_available()
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-model = whisper.load_model("tiny",device=DEVICE)
+model = whisper.load_model("tiny", device=DEVICE)
 
-app=FastAPI()
+app = FastAPI()
 
 @app.post("/whisper/")
 async def handler(files: List[UploadFile] = File(...)):
     if not files:
         raise HTTPException(status_code=400, detail="No files were provided")
     results = []
-
     for file in files:
         with NamedTemporaryFile(delete=True) as temp:
             with open(temp.name, "wb") as temp_file:
@@ -26,9 +25,12 @@ async def handler(files: List[UploadFile] = File(...)):
                 'filename': file.filename,
                 'transcript': result['text'],
             })
-
     return JSONResponse(content={'results': results})
 
 @app.get("/", response_class=RedirectResponse)
 async def redirect_to_docs():
     return "/docs"
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
